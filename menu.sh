6c4a9ed32fd01411f82842783b850960b87a81d9
huaweipadu <97283610+huaweipadu@users.noreply.github.com>
2022-03-21 21:04:03 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -43,9 +43,6 @@ CITY=$(curl -s ipinfo.io/city )
 WKT=$(curl -s ipinfo.io/timezone )
 IPVPS=$(curl -s ipinfo.io/ip )
 domain=$(cat /etc/v2ray/domain)
-jam=$(date +"%T")
-hari=$(date +"%A")
-tnggl=$(date +"%d-%B-%Y")
 Sver=$(cat /home/version)
 tele=$(cat /home/contact)
 	cname=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
@@ -56,17 +53,17 @@ tele=$(cat /home/contact)
 	fram=$( free -m | awk 'NR==2 {print $4}' )
 	swap=$( free -m | awk 'NR==4 {print $2}' )
 clear
-echo -e "$green TIME                 :$CYAN $jam"$NC
-echo -e "$green DAY                  :$CYAN $hari"$NC
-echo -e "$green DATE                 :$CYAN $tnggl"$NC
-echo -e "$green Bash Ver             :$CYAN $versibash"$NC
 echo -e "$green CPU Model            :$CYAN $cname"$NC
+echo -e "$green CPU Frequency        :$CYAN $freq MHz"$NC
+echo -e "$green Number Of Cores      :$CYAN $cores"$NC
+echo -e "$green CPU Usage            :$CYAN $cpu_usage"$NC
+echo -e "$green Operating System     :$CYAN "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`$NC
 echo -e "$green Kernel               :$CYAN `uname -r`"$NC
+echo -e "$green Bash Ver             :$CYAN $versibash"$NC
 echo -e "$green Total Amount Of RAM  :$CYAN $tram MB"$NC
 echo -e "$green Used RAM             :$CYAN $uram MB"$NC
 echo -e "$green Free RAM             :$CYAN $fram MB"$NC
-echo -e "$green Used RAM             :$CYAN $uram MB"$NC
-echo -e "$green Operating System     :$CYAN "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`$NC
+echo -e "$green System Uptime        :$CYAN $uptime $DF( From VPS Booting )"$NC
 echo -e "$green Download             :$CYAN $downloadsize GB ( From Startup / VPS Booting )"$NC
 echo -e "$green Upload               :$CYAN $uploadsize GB ( From Startup / VPS Booting )"$NC
 echo -e "$green ISP NAME             :$CYAN $ISP"$NC
@@ -74,7 +71,9 @@ echo -e "$green IP VPS               :$CYAN $IPVPS"$NC
 echo -e "$green DOMAIN               :$CYAN $domain"$NC
 echo -e "$green City                 :$CYAN $CITY"$NC
 echo -e "$green SERVER               :$CYAN $ISP"$NC
-echo -e "$green Script Version       :$Sver"$NC
+echo -e "$green Client Name          :$CYAN $name${NC}"
+echo -e "$green Expired Date         :$CYAN $exp${NC}"
+echo -e "$green Script Version       :$CYAN $Sver"
 echo -e "${green}════════════════════════════════════════════════════════════${NC}"
 echo -e "${BGBLUE}                     [ PREMIUM SCRIPT ]                     ${NC}"
 echo -e "${green}════════════════════════════════════════════════════════════${NC}"
