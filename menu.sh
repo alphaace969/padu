15867abcc2982581b74056d4331592de48f220e8
huaweipadu <97283610+huaweipadu@users.noreply.github.com>
2022-04-02 01:45:57 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -13,10 +13,9 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl https://raw.githubusercontent.com/angahpadu/ip/main/access | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/huaweipadu/ip/main/access | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
-echo -e ""
 # DNS Patch
 tipeos2=$(uname -m)
 # OS Uptime
